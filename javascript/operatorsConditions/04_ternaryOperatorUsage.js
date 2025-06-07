3d8cbb0982bb0955b8100cf679a32fcd5128b871
RaoVrn <prakash.varun.0305@gmail.com>
2025-06-07 18:25:42 +0530

    🧪 Use ternary operator for age check

---
+++ javascript/operatorsConditions/04_ternaryOperatorUsage.js
@@ -0,0 +1,20 @@
+/*
+🧪 Question 4: Use Ternary Operator for Age Check
+
+Task:
+- Ask user for age
+- Use ternary operator to print:
+  "Adult" if age >= 18
+  "Minor" if age < 18
+
+Topics Covered:
+- Ternary operator (condition ? val1 : val2)
+- prompt()
+- console.log()
+
+Difficulty: 🟢 Beginner
+*/
+
+let age = prompt("Enter the age: ");
+
+age>=18 ? console.log("Adult") : console.log("Minor");
