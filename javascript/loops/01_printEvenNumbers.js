fdb4fbf036ad5053953905284ecb448a536ba03c
RaoVrn <prakash.varun.0305@gmail.com>
2025-06-07 18:48:37 +0530

    ➗ Print all even numbers from 0 to 100

---
+++ javascript/loops/01_printEvenNumbers.js
@@ -0,0 +1,17 @@
+/*
+➗ Question 1: Print Even Numbers from 0 to 100
+
+Task:
+- Use a loop to print all even numbers from 0 to 100 using console.log()
+
+Topics Covered:
+- for loop
+- if condition
+- modulus operator %
+
+Difficulty: 🟢 Beginner
+*/
+
+for(let i=0; i<=100; i++) {
+	console.log("num =", i);
+}
