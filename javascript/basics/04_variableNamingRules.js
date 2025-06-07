67638a741cc87c798cb6f07ddcb76bb51acda37d
RaoVrn <prakash.varun.0305@gmail.com>
2025-06-07 17:47:04 +0530

    ✅ Practice JS variable naming rules

---
+++ javascript/basics/04_variableNamingRules.js
@@ -0,0 +1,39 @@
+/*
+✅ Question 4: Practice Variable Naming Rules
+
+Task:
+Create 5 variables with valid names and assign them some values.
+Also, write 3 examples of invalid variable names as comments.
+
+Valid:
+- userName
+- $balance
+- _score
+- totalMarks
+- age1
+
+Invalid:
+- 1name (starts with digit)
+- user name (contains space)
+- let (reserved keyword)
+
+Topics Covered:
+- Variable naming rules
+- let, const, var usage
+- Comments
+
+Difficulty: 🟢 Beginner
+*/
+
+// valid names
+let username = "Varun";
+let $balance = 1000000;
+let _score = 99;
+let totalMarks = 100;
+let age1 = 20;
+
+// invalid names
+
+// let 1name = "Varun";
+// let user name = "Rao";
+// let var = 1; 
