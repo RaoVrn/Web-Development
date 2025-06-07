5830af4497763a86a037346ae85578b43803706e
RaoVrn <prakash.varun.0305@gmail.com>
2025-06-07 17:32:01 +0530

    🧍‍♂️ Add profile object using const

---
+++ javascript/basics/02_createProfileObject.js
@@ -0,0 +1,31 @@
+/*
+🧍‍♂️ Question 2: Create a Profile Object
+
+Task:
+Create a constant object called 'profile' that stores basic user information.
+
+The object should contain:
+- username: "varun_dev"
+- isFollow: true
+- followers: 500
+- following: 120
+
+After creating the object, use console.log(profile) to print the object.
+
+Topics Covered:
+- const keyword
+- Object creation
+- Boolean, number, and string data types
+- console.log()
+
+Difficulty: 🟢 Beginner
+*/
+
+const profile = {
+	username: "varun_dev",
+	isFollow: true,
+	followers: 500,
+	following: 120
+};
+
+console.log(profile)
