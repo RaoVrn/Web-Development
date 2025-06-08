5b44982d3f966e566eb6f9c9de339a2c3ab74ff8
RaoVrn <prakash.varun.0305@gmail.com>
2025-06-08 20:38:29 +0530

    🔁 Loop through array and print elements

---
+++ javascript/04_arrays/04_loopThroughArray.js
@@ -0,0 +1,19 @@
+/*
+🔁 Question 4: Loop Over an Array
+
+Task:
+Create an array of your 5 favorite superheroes and print each name on a new line.
+
+Topics Covered:
+- Array declaration
+- for loop
+- console.log()
+
+Difficulty: 🟢 Beginner
+*/
+
+let superheroes = ["ram", "lakshman", "hanuman", "krishna", "arjun"];
+
+for(let i=0; i<superheroes.length; i++) {
+	console.log(superheroes[i]);
+}
